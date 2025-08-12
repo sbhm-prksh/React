a998574db6ef816e090a8b38d8e159a9f84d09ee
Shubham Prakash <sbhmprkshsp@gmail.com>
2025-08-12 23:05:00 +0530

    Custom React

---
+++ 02customReact/customreact.js
@@ -0,0 +1,9 @@
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_black'
+    },
+    children: 'Click me to visit google'
+}
+const mainContainer = document.getElementById('root')
